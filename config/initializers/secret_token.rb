215c15df45ed4d63d668a4e3e104532869da8e6d
Uriel Hernández <uriel.hdzc@gmail.com>
2013-12-02 16:48:46 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EasyDesignDev::Application.config.secret_key_base = 'cd205e775e9d4282d13a9b2684e639f007afc727edea32b11fdf41c3728dbc0e5e9661d200506d340052ddd31222b515dea3bfaeac46ac1375d17f76799b7c46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EasyDesignDev::Application.config.session_store :cookie_store, key: '_easyDesign_dev_session'
